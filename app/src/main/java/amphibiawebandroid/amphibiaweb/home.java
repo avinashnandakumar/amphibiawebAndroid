c546a46d727666fd76c1ea6ec5533e97dfaaac2b
Avinash Nandakumar <avinashnandakumar@berkeley.edu>
2018-10-11 03:43:16 -0700

    home page

--- app/src/main/java/amphibiawebandroid/amphibiaweb/home.java
+++ app/src/main/java/amphibiawebandroid/amphibiaweb/home.java
@@ -2,6 +2,7 @@ package amphibiawebandroid.amphibiaweb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
